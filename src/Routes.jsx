b43808fc86701820786304e7bf77f8a56b24d253
M-Harish05 <harish.musidipilli@gmail.com>
2026-01-06 06:25:59 +0530

    Update Routes.jsx

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter, Routes as RouterRoutes, Route } from "react-router-dom";
-import ScrollToTop from "components/ScrollToTop";
-import ProtectedRoute from "components/ProtectedRoute";
-import ErrorBoundary from "components/ErrorBoundary";
+import ScrollToTop from "./components/ScrollToTop";
+import ProtectedRoute from "./components/ProtectedRoute";
+import ErrorBoundary from "./components/ErrorBoundary";
 import NotFound from "pages/NotFound";
 import BasicLiteracyLearning from './pages/basic-literacy-learning';
 import ProgressTracking from './pages/progress-tracking';
